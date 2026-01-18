b0a2e528505d2ca5b062edb8204ac5e8a7e72850
MarcheOmbre <victor.pr.liu@gmail.com>
2026-01-18 04:20:42 +0100

    fix reset password script

--- ResetPassword/script.js
+++ ResetPassword/script.js
@@ -4,19 +4,16 @@ const resetButton = document.getElementById("reset_button");
 const alert = document.getElementById("alert");
 
 class ResetPasswordDto {
-    constructor(email, password) {
-        
-        if(!isMatchingMailPattern(email))
-            throw new Error("Invalid email pattern");
+    constructor(password, passwordConfirmation) {
         
         if(!isString(password))
             throw new Error("Password must be a string");
         
-        if(password === "")
-            throw new Error("Password can't be null");
+        if(password !== passwordConfirmation)
+            throw new Error("Passwords doesn't match");
         
         this.password = password;
-        this.passwordConfirmation = password;
+        this.passwordConfirmation = passwordConfirmation;
     }
 }
 
@@ -45,6 +42,12 @@ document.addEventListener("DOMContentLoaded", async function () {
             throw new Error("Token must be a string");
 
         resetButton.disabled = true;
+        
+        if(!isString(passwordInput.value) || passwordInput.value.trim() === "")
+            alert.textContent = "Password can't be empty";
+        
+        if(passwordInput.value !== passwordConfirmationInput.value)
+            alert.textContent = "Passwords doesn't match";
         
         await SendRequest("POST", token, null,
             APILink + "Authentification/ResetPassword",
