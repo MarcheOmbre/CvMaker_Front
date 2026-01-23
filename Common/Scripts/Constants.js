d0aff2001016e5eb354e4190ecbb98ce7838ca3f
MarcheOmbre <victor.pr.liu@gmail.com>
2026-01-23 22:56:24 +0100

    fix

--- Common/Scripts/Constants.js
+++ Common/Scripts/Constants.js
@@ -1,4 +1,4 @@
-const APILink = "http://localhost:5000/";// = "https://cvbuilderbackapi.azurewebsites.net/";
+const APILink = "https://cvbuilderbackapi.azurewebsites.net/";
 const CvIdItemKey = "CvData";
 const TokenKey = "Token";
 
